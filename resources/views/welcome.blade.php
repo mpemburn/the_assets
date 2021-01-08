1876f62845c2c86464359c5c4a0f6838da6434ea
Mark Pemburn <mark@pemburn.com>
2021-01-08 07:40:29 -0500

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<script>
+    import { fabric } from "fabric";
+    var canvas = new fabric.Rect({
+        left: 100,
+        top: 100,
+        fill: 'red',
+        width: 20,
+        height: 20
+    });
+
+    canvas.add(rect);
+
+</script>
