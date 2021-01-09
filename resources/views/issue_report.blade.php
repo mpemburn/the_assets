44c78e0616aa521f76e7d4e953ec264677da1e52
Mark Pemburn <mark@pemburn.com>
2021-01-09 15:56:29 -0500

    Fix HTML in report Blade

--- resources/views/issue_report.blade.php
+++ resources/views/issue_report.blade.php
@@ -1,29 +1,40 @@
+<style>
+    td.critical {
+        background-color: red;
+    }
+    td.major {
+        background-color: orange;
+    }
+    td.minor {
+        background-color: yellow;
+    }
+</style>
 <table>
 @foreach($issues as $issue)
     <tr>
-        <td>{!! $issue[0] !!}</td>
-        <td>{!! $issue[1] !!}</td>
-        <td>{!! $issue[3] !!}</td>
+        <td class="{!! $issue[0] !!}">{!! $issue[0] !!}</td>
+        <td class="{!! $issue[0] !!}">{!! $issue[1] !!}</td>
+        <td class="{!! $issue[0] !!}">{!! $issue[3] !!}</td>
     </tr>
     @if($issueReport->hasAffectedDevices($issue[2]))
     <tr>
-        <table>
-            @foreach($issueReport->getAffectedDevices($issue[2])->toArray() as $key => $device)
-                <tr>
-                    <td width="20px">&nbsp;</td>
-                    <td>
-                        {!! current($device) !!}
-                    </td>
-                </tr>
-                <tr>
-                    <td width="20px">&nbsp;</td>
-                    <td>
-                        ({!! $inventory->getDeviceString(key($device)) !!})
-                    </td>
-                </tr>
+        <td colspan="3">
+            <table>
+                @foreach($issueReport->getAffectedDevices($issue[2])->toArray() as $key => $device)
+                    <tr>
+                        <td style="padding-left: 30px;">
+                            {!! current($device) !!}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td style="padding-left: 30px;">
+                            ({!! $inventory->getDeviceString(key($device)) !!})
+                        </td>
+                    </tr>
 
-            @endforeach
-        </table>
+                @endforeach
+            </table>
+        </td>
     </tr>
     @endif
 @endforeach
